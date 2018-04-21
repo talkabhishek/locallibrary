9921d4342cfca79d9837406c56f410deabe3317a
Abhishek Singh <talk.abhisheksingh@gmail.com>
2018-04-21 17:19:49 +0530

    routes setup

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var wiki = require("./routes/wiki.js");
+var catalog = require('./routes/catalog');  //Import routes for "catalog" area of site
 
 var app = express();
 
@@ -34,6 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use("/wiki", wiki);
+app.use('/catalog', catalog);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/express', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get("/", function(req, res, next) {
+  res.redirect("/catalog");
 });
 
 module.exports = router;
